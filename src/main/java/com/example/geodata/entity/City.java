a073c798bfcbd5bf338c9129fbcb149f09bb4ee7
limbo <ilia.mesha44@gmail.com>
2024-03-07 19:27:22 +0300

    fix put

--- src/main/java/com/example/geodata/entity/City.java
+++ src/main/java/com/example/geodata/entity/City.java
@@ -1,8 +1,6 @@
 package com.example.geodata.entity;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.*;
 
--- src/main/java/com/example/geodata/repository/CityRepository.java
+++ src/main/java/com/example/geodata/repository/CityRepository.java
@@ -2,11 +2,8 @@ package com.example.geodata.repository;
 
 import com.example.geodata.entity.City;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
