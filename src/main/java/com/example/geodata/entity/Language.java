ffa07c04653b20f8c965ae15a78a0aa042a696be
limbo <ilia.mesha44@gmail.com>
2024-03-07 15:41:22 +0300

    fix lombock

--- src/main/java/com/example/geodata/entity/Language.java
+++ src/main/java/com/example/geodata/entity/Language.java
@@ -20,10 +20,10 @@ public class Language {
     private Integer id;
 
     @Column(name = "language_name")
-    private String nameLanguage;
+    private String name;
 
     @Column(name = "language_code")
-    private String codeLanguage;
+    private String code;
 
     @ManyToMany(mappedBy = "languages", cascade = { CascadeType.MERGE, CascadeType.PERSIST }, fetch = FetchType.LAZY)
     @JsonBackReference
