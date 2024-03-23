d928521e77cd0fee55c7ca823b43a8f8c5c99e5d
limbo <ilia.mesha44@gmail.com>
2024-03-23 12:24:05 +0300

    edit LoggingAspect

---
+++ src/main/java/com/example/geodata/aspects/LoggingAspect.java
@@ -0,0 +1,49 @@
+package com.example.geodata.aspects;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Aspect
+@Component
+@Slf4j
+public class LoggingAspect {
+
+    @Pointcut("within(com.example.geodata.controller..*)" +
+            " || within(com.example.geodata.service..*)" +
+            " || within(com.example.geodata.repository..*)" +
+            " || within(com.example.geodata.cache..*)")
+    public void loggingController() {
+
+    }
+
+    @Around("loggingController()")
+    public Object logEnteringAPI(ProceedingJoinPoint joinPoint) throws Throwable {
+        log.info("Enter: {}.{}() with argument[s] = {}", joinPoint.getSignature().getDeclaringTypeName(),
+                joinPoint.getSignature().getName(), Arrays.toString(joinPoint.getArgs()));
+        try {
+            Object result = joinPoint.proceed();
+            log.info("Exit: {}.{}() with result = {}", joinPoint.getSignature().getDeclaringTypeName(),
+                    joinPoint.getSignature().getName(), result);
+            return result;
+        }catch (IllegalArgumentException e){
+            log.error("Illegal argument: {} in {}.{}()", Arrays.toString(joinPoint.getArgs()),
+                    joinPoint.getSignature().getDeclaringTypeName(), joinPoint.getSignature().getName());
+            throw e;
+        }
+    }
+
+    @AfterThrowing(pointcut = "loggingController()", throwing = "exception")
+    public void logsExceptionsFromAnyLocation(JoinPoint joinPoint, Throwable exception) {
+        log.error("Exception in : {}.{}() cause = {}", joinPoint.getSignature().getDeclaringTypeName(),
+                joinPoint.getSignature().getName(), exception.getMessage());
+    }
+
+}
