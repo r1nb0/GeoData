de3a19247829d1cc1a885cad9b19db8982351c15
limbo <ilia.mesha44@gmail.com>
2024-04-01 23:40:13 +0300

    add @Builder into DTO's

--- src/main/java/com/example/geodata/dto/CountryDTO.java
+++ src/main/java/com/example/geodata/dto/CountryDTO.java
@@ -1,7 +1,10 @@
 package com.example.geodata.dto;
 
+import lombok.Builder;
+
 import java.util.List;
 
+@Builder
 public record CountryDTO(Integer id, String name,
                          String nationality, Double latitude,
                          Double longitude, List<String> languages) {
