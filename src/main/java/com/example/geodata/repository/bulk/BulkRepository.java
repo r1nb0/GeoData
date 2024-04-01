e3ba54200fb601ed2740fa4c0ac0b5a46b0b3c99
r1nb0 <159806721+r1nb0@users.noreply.github.com>
2024-04-01 23:51:16 +0300

    Update BulkRepository.java

--- src/main/java/com/example/geodata/repository/bulk/BulkRepository.java
+++ src/main/java/com/example/geodata/repository/bulk/BulkRepository.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Collection;
 
 @Repository
-public interface BulkRepository<T, E> {
+public interface BulkRepository<E> {
 
     void bulkInsert(Collection<E> entities);
 
