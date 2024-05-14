d14c319e02fb0bcee11beddd716a1435a723926a
limbo <ilia.mesha44@gmail.com>
2024-05-14 16:03:33 +0300

    +edit tests

---
+++ src/test/java/TestSuite.java
@@ -0,0 +1,7 @@
+import com.example.geodata.GeoDataApplicationTests;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = {GeoDataApplicationTests.class})
+public class TestSuite {
+
+}
--- src/test/java/com/example/geodata/GeoDataApplicationTests.java
+++ src/test/java/com/example/geodata/GeoDataApplicationTests.java
@@ -1,11 +1,8 @@
 package com.example.geodata;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = GeoDataApplicationTests.class)
-class GeoDataApplicationTests {
-
+public class GeoDataApplicationTests {
 
 	@Test
 	void contextLoads() {
