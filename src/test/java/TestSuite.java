889167b016c7a4ea624b2141af15c236113e9ba1
limbo <ilia.mesha44@gmail.com>
2024-05-14 16:08:54 +0300

    +edit tests

--- src/test/java/TestSuite.java
+++
@@ -1,7 +0,0 @@
-import com.example.geodata.GeoDataApplicationTests;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest(classes = {GeoDataApplicationTests.class})
-public class TestSuite {
-
-}
--- src/test/java/com/example/geodata/GeoDataApplicationTests.java
+++ src/test/java/com/example/geodata/GeoDataApplicationTests.java
@@ -1,12 +1,18 @@
 package com.example.geodata;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
-public class GeoDataApplicationTests {
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest(classes = {GeoDataApplicationTests.class})
+class GeoDataApplicationTests {
+
 
 	@Test
 	void contextLoads() {
-
+        String asd = "sonarqube";
+        assertNotNull(asd);
 	}
 
 }
